3e2daf800f3c37d17099b0b2a494760b99f34bda
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-08-25 18:05:28 -0700

    Recursively fetch sections

--- src/data/utilsQr.js
+++ src/data/utilsQr.js
@@ -23,26 +23,49 @@ export function userDataToQr(userData) {
     };
 }
 
+
 export async function qrToUserData(qrString) {
-    // TODO: move settimeout here
     const qrJson = JSON.parse(qrString);
     let courses = qrJson.courses;
-    const sectionPromises = courses.map(section => fetchSectionData(section));
-    const sectionData = await Promise.all(sectionPromises);
+    courses = courses.map(e => ({name: e, location: "", classTimes: [], notFetched: true}))
+    const sectionData = await fetchSections(courses);
+    console.log("promised data", sectionData);
     return {
         name: qrJson.name,
-        courses: sectionData.filter(e => e !== null)
+        courses: sectionData
     }
 }
 
-async function fetchSectionData(section) {
-    const sectionURL = section.replaceAll(" ", "-");
+async function fetchSections(sections, recursionCount = 0) {
+    if (recursionCount >= 4) {
+        // TODO: error toast
+        console.error("Recursion limit reached. Returning data as is.");
+        return sections;
+    }
+
+    const sectionPromises = sections.map(async section => {
+        if (section.notFetched) {
+            const sectionURL = section.name.replaceAll(" ", "-");
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            const sectionInfo = await fetchSectionInfo(sectionURL, section);
+            return sectionInfo;
+        } else {
+            return section;
+        }
+    });
+    const sectionData = await Promise.all(sectionPromises);
+    if (sectionData.some(section => section.notFetched)) {
+        return fetchSections(sectionData, recursionCount + 1);
+    } else {
+        return sectionData;
+    }
+}
+
+async function fetchSectionInfo(sectionURL, section) {
     try {
-        // TODO: make sure this delay works always
-        await new Promise(resolve => setTimeout(resolve, 1000));
         const response = await fetch(API_LINK + ENDPOINTS.sectionInfo + sectionURL);
         return await response.json();
     } catch (e) {
-        return {name: section, location: "", classTimes: [], notFetched: true};
+        return section;
     }
 }
