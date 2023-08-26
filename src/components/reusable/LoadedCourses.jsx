df8171806de18b98ef7a1845134eea6aa60a5cdb
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-08-25 19:49:17 -0700

    UI improvements

--- src/components/reusable/LoadedCourses.jsx
+++ src/components/reusable/LoadedCourses.jsx
@@ -5,29 +5,36 @@ import {Divider, Stack, Typography} from "@mui/material";
 // TODO: placeholder when no courses loaded yet
 // TODO: refactor, change handleDelete to setCourses. add delete={true/false} prop. handle deletion internally
 export default function LoadedCourses({courses, handleDelete}) {
-    return (
-        <Stack
-            sx={{height: '100%', overflowY: 'auto'}}
-            direction={'column'}
-            spacing={1}
-            divider={<Divider
-                variant={'fullWidth'}
-                orientation="horizontal"
-                flexItem
-            />}
-        >
-            {courses && courses?.length > 0
-                ? courses.map(({name, location, friends, classTimes}, i) =>
-                    <CourseBlock
-                        key={`course-${name}`}
-                        course={name}
-                        location={location}
-                        friends={friends}
-                        classTimes={classTimes}
-                        handleDelete={handleDelete ? () => handleDelete(i) : null}
-                    />
-                )
-                : <Typography>No courses loaded</Typography>}
-        </Stack>
+    return (courses && courses?.length > 0
+            ? <Stack
+                sx={{height: '100%', overflowY: 'hidden'}}
+                spacing={2}
+            >
+                <Typography>
+                    {courses.length} sections loaded
+                </Typography>
+                <Stack
+                    sx={{height: '100%', overflowY: 'auto'}}
+                    direction={'column'}
+                    spacing={1}
+                    divider={<Divider
+                        variant={'fullWidth'}
+                        orientation="horizontal"
+                        flexItem
+                    />}
+                >
+                    {courses.map(({name, location, friends, classTimes}, i) =>
+                        <CourseBlock
+                            key={`course-${name}`}
+                            course={name}
+                            location={location}
+                            friends={friends}
+                            classTimes={classTimes}
+                            handleDelete={handleDelete ? () => handleDelete(i) : null}
+                        />
+                    )}
+                </Stack>
+            </Stack>
+            : <Typography>No courses loaded</Typography>
     )
 }
